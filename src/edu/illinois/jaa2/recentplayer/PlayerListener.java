852ebb04146a4b09a328e08ac8f5988833ab5858
Jim <jaa2@illinois.edu>
2019-03-21 17:17:08 -0500

    Initial commit

---
+++ src/edu/illinois/jaa2/recentplayer/PlayerListener.java
@@ -0,0 +1,87 @@
+package edu.illinois.jaa2.recentplayer;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.OptionalLong;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.ocpsoft.prettytime.PrettyTime;
+
+public class PlayerListener implements Listener {
+	
+	private Map<UUID, Long> playerList;
+	private Double groupingWindow;
+	private long messageMaxPlayers;
+	private Locale locale;
+	
+	public PlayerListener(Map<UUID, Long> playerList, FileConfiguration config) {
+		this.playerList = playerList;
+		
+		// Load config
+		groupingWindow = config.getDouble("player-grouping-window", 10L);
+		locale = new Locale(config.getString("locale", "en"));
+		messageMaxPlayers = config.getInt("message-max-players", 1);
+	}
+	
+	
+	/**
+	 * Gets the list of last players
+	 * @return list of last players
+	 */
+	public Map<UUID, Long> getLastPlayers() {
+		return playerList;
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		addPlayerToList(event.getPlayer());
+		event.getPlayer().getServer().getLogger().info("Added " + event.getPlayer().getName() + " to the list");
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		filterPlayerList();
+		if (event.getPlayer().getServer().getOnlinePlayers().size() <= messageMaxPlayers && playerList.size() > 0) {
+			String playerListStr = "";
+			List<String> playerNameList = playerList.keySet().stream()
+					.map(playerUUID -> event.getPlayer().getServer().getPlayer(playerUUID).getName())
+					.collect(Collectors.toList());
+			
+			if (playerNameList.size() == 1) {
+				playerListStr = playerNameList.get(0) + " was";
+			} else if (playerNameList.size() >= 2) {
+				playerListStr = playerNameList.subList(0, playerNameList.size() - 1).stream()
+						.collect(Collectors.joining(", "))
+						+ (playerNameList.size() > 2 ? "," : "") + " and " + playerNameList.get(playerNameList.size() - 1)
+						+ " were";
+			}
+			
+			OptionalLong latestTime = playerList.values().stream().mapToLong(date -> date).max();
+			if (latestTime.isPresent()) {
+				PrettyTime prettyTime = new PrettyTime(locale);
+				event.getPlayer().sendMessage(ChatColor.GREEN + playerListStr + " last online " + prettyTime.format(new Date(latestTime.getAsLong())));
+			}
+		}
+	}
+	
+	public void addPlayerToList(OfflinePlayer player) {
+		Date windowStart = new Date((long) (System.currentTimeMillis() - 1000 * 60 * 60 * groupingWindow));
+		playerList.keySet().removeIf(offp -> (playerList.get(offp) < windowStart.getTime()));
+		playerList.put(player.getUniqueId(), System.currentTimeMillis());
+	}
+	
+	public void filterPlayerList() {
+		playerList.keySet().removeIf(playerUUID -> Bukkit.getServer().getOfflinePlayer(playerUUID).isOnline());
+	}
+}
---
+++ src/edu/illinois/jaa2/recentplayer/RecentPlayer.java
@@ -0,0 +1,111 @@
+package edu.illinois.jaa2.recentplayer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RecentPlayer extends JavaPlugin {
+	
+	private PlayerListener playerListener;
+	
+	@Override
+	public void onEnable() {
+		// Load the description file to get the plugin's name and version
+		PluginDescriptionFile pdfFile = this.getDescription();
+		Bukkit.getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " has been enabled!"); 
+		
+		Map<UUID, Long> lastPlayers = loadLastPlayers();
+		playerListener = new PlayerListener(lastPlayers, getConfig());
+		getServer().getPluginManager().registerEvents(playerListener, this);
+	}
+	
+	@Override
+	public void onDisable() {
+		saveLastPlayers(playerListener.getLastPlayers());
+		
+		//Load the description file to get the plugin's name and version
+		PluginDescriptionFile pdfFile = this.getDescription();
+		Bukkit.getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " has been disabled!");
+	}
+	
+	/**
+	 * Saves the players who were last online
+	 * @param playerList list of players and their logout times
+	 */
+	public void saveLastPlayers(Map<UUID, Long> lastPlayers) {
+		File lastPlayersFile = new File(getDataFolder(), "lastPlayers.yml");
+		FileConfiguration lastPlayersConfig = YamlConfiguration.loadConfiguration(lastPlayersFile);
+		
+		lastPlayersConfig.set("lastPlayers", null);
+		ConfigurationSection playersSection = lastPlayersConfig.createSection("lastPlayers");
+		
+		List<UUID> players = lastPlayers.keySet().stream().collect(Collectors.toList());
+		for (int i = 0; i < players.size(); i++) {
+			ConfigurationSection thisPlayerSection = playersSection.createSection(Integer.toString(i));
+			thisPlayerSection.set("player", players.get(i));
+			thisPlayerSection.set("lastQuit", lastPlayers.get(players.get(i)));
+		}
+		
+		try {
+			lastPlayersConfig.save(lastPlayersFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		getLogger().log(Level.INFO, "Saved " + lastPlayers.size() + " last players");
+	}
+	
+	/**
+	 * Loads the players who were online last
+	 * @return map of OfflinePlayer to the (Long) time he or she was online last
+	 */
+	public Map<UUID, Long> loadLastPlayers() {
+		// Load the YML file with the last players
+		File lastPlayersFile = new File(getDataFolder(), "lastPlayers.yml");
+		YamlConfiguration lastPlayersConfig = YamlConfiguration.loadConfiguration(lastPlayersFile);
+
+		Map<UUID, Long> lastPlayers = new HashMap<UUID, Long>();
+		
+		if (lastPlayersConfig.isSet("lastPlayers")) {
+			ConfigurationSection lastPlayersSection = lastPlayersConfig.getConfigurationSection("lastPlayers");
+			for (String key : lastPlayersSection.getKeys(false)) {
+				ConfigurationSection playerSection = lastPlayersSection.getConfigurationSection(key);
+				
+				if (!playerSection.contains("player")) {
+					getLogger().warning("'player' property not found in one of the entries!");
+					continue;
+				}
+				
+				try {
+					UUID player = UUID.fromString(playerSection.getString("player"));
+					Long lastQuitTime = playerSection.getLong("lastQuit", -1);
+					
+					if (lastQuitTime != -1 && player != null) {
+						lastPlayers.put(player, lastQuitTime);
+					} else {
+						getLogger().info("Uh-oh: last quit time is " + lastQuitTime + " and offline player is " + player);
+					}
+				} catch (IllegalArgumentException e) {
+					getLogger().warning("UUID is not formed properly: " + playerSection.getString("player"));
+				}
+			}
+		} else {
+			getLogger().info("Last players not set");
+		}
+		
+		getLogger().log(Level.INFO, "Loaded " + lastPlayers.size() + " players who were on last.");
+		return lastPlayers;
+	}
+}
